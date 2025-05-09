b4848c4c351c3dcb90f57de2d7da99934695bddb
Anton_Yakovlev <anton.iakovlev1@gmail.com>
2025-05-09 18:14:05 +0300

    file IfElseStatementTheme.java corrected

--- Lesson1/CyclesTheme.java
+++ Lesson1/CyclesTheme.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class CyclesTheme {
     public static void main(String[] args) {
         System.out.println("1. ВЫВОД ASCII-СИМВОЛОВ");
@@ -43,6 +45,47 @@ public class CyclesTheme {
                 }
             }
             System.out.println(' ');
+        }
+
+        System.out.println("\n4. ВЫВОД ЧИСЕЛ В НЕСКОЛЬКО СТРОК");
+
+        /* Работающая версия кода, но до конца не понял почему (подсмотрено на https://translated.turbopages
+                .org/proxy_u/en-ru.ru.83757b9f-68175b5f-6510e8cf-74722d776562/https/stackoverflow
+                .com/questions/27092911/display-five-numbers-per-line-in-java)
+        */
+        /*
+        int upper = 24;
+        for (int i = 1; i <= upper; i++) {
+            for (int j = 0; j < 5; i++) {
+                if ((i % 2 != 0) && i < upper) {
+                    System.out.printf("%2d%s", i, " ");
+                    j++;
+                } else if (i > upper) {
+                    System.out.printf("%2d%s", 0, " ");
+                    j++;
+                }
+            }
+            System.out.println();
+        }
+        */
+
+        int upper = 24;
+        int count = 0;
+        for (int i = 1; i <= upper; i++) {
+            if (i % 2 != 0) {
+                System.out.printf("%2d%s", i, " ");
+                count++;
+                if (count % 5 == 0) {
+                    System.out.println();
+                }
+            }
+        }
+        if (count % 5 != 0) {
+            int remaining = 5 - (count % 5);
+            for (int i = 0; i < remaining; i++) {
+                System.out.printf("%2d%s", 0, " ");
+            }
+            System.out.println();
         }
     }
 }
