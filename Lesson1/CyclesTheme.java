da6f0707b687fc5b444fe8e44eecf324d3a9dd64
Anton_Yakovlev <anton.iakovlev1@gmail.com>
2025-05-24 11:30:12 +0300

    Cycles homework finished

--- Lesson1/CyclesTheme.java
+++ Lesson1/CyclesTheme.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+import java.util.Random;
 
 public class CyclesTheme {
     public static void main(String[] args) {
@@ -41,7 +41,7 @@ public class CyclesTheme {
                 } else if (i == 1) {
                     System.out.print("---");
                 } else {
-                    System.out.printf("%3s", i*j);
+                    System.out.printf("%3s", i * j);
                 }
             }
             System.out.println(' ');
@@ -109,24 +109,91 @@ public class CyclesTheme {
         for (int i = max - 1; i >= min + 1; i--) {
             System.out.printf("%s ", i);
         }
+        System.out.println();
 
         System.out.println("\n6. РАЗНЫЕ ОПЕРАЦИИ НАД ЧИСЛОМ");
-        int number = 2234321;
-        System.out.println("Исходное число: " + number);
+        int initialNumber = 1232324;
+        int reserveNumber = initialNumber;
         int reversed = 0;
         int twosNumber = 0;
-        while (number != 0) {
-            int digit = number % 10;
+        while (initialNumber != 0) {
+            int digit = initialNumber % 10;
             if (digit == 2) {
                 twosNumber++;
             }
             reversed = reversed * 10 + digit;
-            number /= 10;
+            initialNumber /= 10;
         }
-        System.out.println("Число в обратном порядке: " + reversed);
-        System.out.println("Количество двоек: " + twosNumber);
-        boolean isPalindrome = number == reversed;
+        boolean isPalindrome = reserveNumber == reversed;
         boolean isTwosNumberEven = (twosNumber % 2) == 0;
-        System.out.printf("%s - %s %s (%s) количеством двоек\n", reversed, isPalindrome ? "палиндром" : "не палиндром", isTwosNumberEven ? "с четным" : "с нечетным", twosNumber);
+        System.out.printf("%s - %s %s (%s) количеством двоек\n", reversed,
+                isPalindrome ? "палиндром" : "не палиндром",
+                isTwosNumberEven ? "с четным" : "с нечетным", twosNumber);
+
+        System.out.println("\n7. ПРОВЕРКА СЧАСТЛИВОГО ЧИСЛА");
+        int originalNumber = 101002;
+        int numLength = (int) (Math.log10(originalNumber) + 1);
+        int firtsThreeDigits = originalNumber / (int) Math.pow(10, (numLength - 3));
+        int firtsThreeDigitsSum = 0;
+        while (firtsThreeDigits != 0) {
+            int oneDigit = firtsThreeDigits % 10;
+            firtsThreeDigitsSum += oneDigit;
+            firtsThreeDigits /= 10;
+        }
+        int tempNum = originalNumber;
+        int reversedNum = 0;
+        while (tempNum != 0) {
+            int oneDigit = tempNum % 10;
+            reversedNum = reversedNum * 10 + oneDigit;
+            tempNum /= 10;
+        }
+        int lastThreeDigits = reversedNum / (int) Math.pow(10, (numLength - 3));
+        int lastThreeDigitsSum = 0;
+        while (lastThreeDigits != 0) {
+            int oneDigit = lastThreeDigits % 10;
+            lastThreeDigitsSum += oneDigit;
+            lastThreeDigits /= 10;
+        }
+        System.out.println(originalNumber + " - " + (firtsThreeDigitsSum == lastThreeDigitsSum ?
+                "счастливое число" : "несчастливое число"));
+        System.out.printf("Сумма цифр %03d = %s\n", originalNumber % (int) Math.pow(10, (numLength - 3)),
+                lastThreeDigitsSum);
+        System.out.printf("Сумма цифр %03d = %s\n", originalNumber / (int) Math.pow(10, (numLength - 3)),
+                firtsThreeDigitsSum);
+
+        System.out.println("\n8. ГЕНЕРАТОР ПАРОЛЯ");
+        String password = "";
+        int totalSymbolsCounter = 0;
+        int digitsCounter = 0;
+        int specialSymbolCounter = 0;
+        int smallWordsCounter = 0;
+        int bigWordsCounter = 0;
+        for (int i = 0; i < 8; i++) {
+            char symbol = (char) (new Random().nextInt(33, 126) + 1);
+            if (symbol >= '0' && symbol <= '9') {
+                digitsCounter++;
+            }
+            if ((symbol >= '!' && symbol <= '/') || (symbol >= ':' && symbol <= '@') ||
+                    (symbol >= '[' && symbol <= '`') || (symbol >= '{' && symbol <= '~')) {
+                specialSymbolCounter++;
+            }
+            if (symbol >= 'a' && symbol <= 'z') {
+                smallWordsCounter++;
+            }
+            if (symbol >= 'A' && symbol <= 'Z') {
+                bigWordsCounter++;
+            }
+            totalSymbolsCounter++;
+            password += symbol;
+        }
+        System.out.println("Пароль: " + password);
+        if (totalSymbolsCounter >= 8 && smallWordsCounter > 0 &&
+                bigWordsCounter > 0 && specialSymbolCounter > 0) {
+            System.out.println("Надежность: Надежный");
+        } else if (totalSymbolsCounter >= 8 && (bigWordsCounter > 0 || digitsCounter > 0)) {
+            System.out.println("Надежность: Средний");
+        } else {
+            System.out.println("Надежность: Слабый");
+        }
     }
 }
