6bf4d06434b3cefabcf15d826b56c0567dd36b9a
Anton_Yakovlev <anton.iakovlev1@gmail.com>
2025-09-29 22:31:51 +0300

    Game remade

--- Lesson2/Player.java
+++ Lesson2/Player.java
@@ -1,21 +1,8 @@
 public class Player {
     private String name;
-    private int number;
 
     public Player(String name) {
         this.name = name;
-    }
-
-    public void setNumber(int number) {
-        if (number > 0 && number <= 100) {
-            this.number = number;
-        } else {
-            System.out.println("Ошибка: число должно быть в диапазоне от 1 до 100");
-        }
-    }
-
-    public int getNumber() {
-        return number;
     }
 
     public String getName() {
