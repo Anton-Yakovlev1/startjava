8ffd1e4bc9a7b0b3cec1f0a8f63b7bf70c7c8973
Anton_Yakovlev <anton.iakovlev1@gmail.com>
2025-10-02 20:40:41 +0300

    Game remade

---
+++ Lesson2/NonBooleanMethods.java
@@ -0,0 +1,8 @@
+public class NonBooleanMethods {
+    public void findLongestWordInSentence() {
+    System.out.println(получитьИмяТекущегоМетода() +
+            "() -> найти самое длинное слово в предложении из книги по Java");
+    }
+
+
+}
