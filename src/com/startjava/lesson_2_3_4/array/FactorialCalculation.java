4b1fb2e38f45545ac840cf944b786d97a018826b
Anton_Yakovlev <anton.iakovlev1@gmail.com>
2026-01-21 23:53:32 +0300

    factorial changes

--- src/com/startjava/lesson_2_3_4/array/FactorialCalculation.java
+++ src/com/startjava/lesson_2_3_4/array/FactorialCalculation.java
@@ -1,6 +1,15 @@
 package com.startjava.lesson_2_3_4.array;
 
+import java.util.Arrays;
+
 public class FactorialCalculation {
+    public static void main(String[] args) {
+        calculateFactorials();
+        calculateFactorials(null);
+        calculateFactorials(-5);
+        calculateFactorials(21, 0, 7);
+        calculateFactorials(1, 20, 5, -3);
+    }
 
     public static long factorial(int number) {
         if (number == 0 || number == 1) return 1;
@@ -22,27 +31,15 @@ public class FactorialCalculation {
         System.out.println(number + "! = " + formula + result);
     }
 
-    /*public static void addResult(long result, countResult) {
-        long[] factorials = new long[countResult.length];
-        int countValidNumbers = 0;
-        for (int i = 0; i < countResult.length; i++) {
-            //int number = numbers[i];
-            factorials[countValidNumbers++] = result;
-        }
-
-        for (long fact : factorials) {
-            System.out.println(fact);
-        }
-    }*/
-
     public static void calculateFactorials(int... numbers) {
         if (numbers == null) {
             System.out.println("Ошибка: передан недействительный массив!");
             return;
         }
 
-        //int countResult = 0;
-        //int newArrayLength = 0;
+        //long[] results = new long[numbers.length];
+        //int index = 0;
+
         for (int number : numbers) {
             if (number < 0) {
                 System.out.println("Ошибка: факториал " + number + "! не определен");
@@ -50,20 +47,10 @@ public class FactorialCalculation {
                 System.out.println("Ошибка: факториал " + number + "! слишком велик (максимум 20!)");
             } else {
                 long result = factorial(number);
-                //countResult++;
+                //results[index++] = result;
                 printResult(number, result);
-                //addResult(result, numbers);
             }
         }
-        //newArrayLength += countResult;
-        //System.out.println(newArrayLength);
-    }
-
-    public static void main(String[] args) {
-        calculateFactorials();               // пустой массив
-        calculateFactorials(null);           // null-значение
-        calculateFactorials(-5);             // отрицательное число
-        calculateFactorials(21, 0, 7);      // смешанное значение, одно большое
-        calculateFactorials(1, 20, 5, -3);  // ещё одна смесь значений
+        //System.out.println(Arrays.toString(results));
     }
 }
