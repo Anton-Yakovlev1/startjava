038b4d79d3ca547cc99f623ba68f16c2e1540a83
Anton_Yakovlev <anton.iakovlev1@gmail.com>
2026-01-24 22:31:04 +0300

    reverser changes

--- src/com/startjava/lesson_2_3_4/array/BankTransactionsReverser.java
+++ src/com/startjava/lesson_2_3_4/array/BankTransactionsReverser.java
@@ -3,40 +3,14 @@ package com.startjava.lesson_2_3_4.array;
 import java.util.Arrays;
 
 public class BankTransactionsReverser {
-    public static int[] reversed;
-
     public static void main(String[] args) {
-        final int[] transactions1 = new int[0];
-        final int[] transactions2 = null;
-        final int[] transactions3 = {5};
-        final int[] transactions4 = {6, 8, 9, 1};
-        final int[] transactions5 = {13, 8, 5, 3, 2, 1, 1};
+        int[][] original = {new int[0], null, {5}, {6, 8, 9, 1}, {13, 8, 5, 3, 2, 1, 1}};
+        int[] reversed;
 
-        reversed = reverse(transactions1);
-        displayTransactions(transactions1, reversed);
-
-        reversed = reverse(transactions2);
-        displayTransactions(transactions2, reversed);
-
-        reversed = reverse(transactions3);
-        displayTransactions(transactions3, reversed);
-
-        reversed = reverse(transactions4);
-        displayTransactions(transactions4, reversed);
-
-        reversed = reverse(transactions5);
-        displayTransactions(transactions5, reversed);
-    }
-
-    private static void displayTransactions(int[] transactions, int[] reversed) {
-        System.out.print("\nИсходные транзакции: ");
-        printTransactions(transactions);
-        System.out.print(" В обратном порядке: ");
-        printTransactions(reversed);
-    }
-
-    private static void printTransactions(int[] transactions) {
-        System.out.println(Arrays.toString(transactions));
+        for (int[] transaction : original) {
+            reversed = reverse(transaction);
+            displayTransactions(transaction, reversed);
+        }
     }
 
     private static int[] reverse(int[] transactions) {
@@ -50,5 +24,10 @@ public class BankTransactionsReverser {
             reversed[--index] = transaction;
         }
         return reversed;
+    }
+
+    private static void displayTransactions(int[] transactions, int[] reversed) {
+        System.out.println("\nИсходные транзакции: " + Arrays.toString(transactions));
+        System.out.println(" В обратном порядке: " + Arrays.toString(reversed));
     }
 }
