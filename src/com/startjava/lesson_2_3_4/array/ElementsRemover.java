75bd12e03f1e148fbdda1d9c177f31afe26025b5
Anton_Yakovlev <anton.iakovlev1@gmail.com>
2026-02-23 00:15:41 +0300

    elements remover change

--- src/com/startjava/lesson_2_3_4/array/ElementsRemover.java
+++ src/com/startjava/lesson_2_3_4/array/ElementsRemover.java
@@ -5,12 +5,27 @@ import java.util.Random;
 
 public class ElementsRemover {
     public static void main(String[] args) {
-        double[] mass = fillWithRandoms(15);
+        int size = 15;
+        int index = -1;
+        double[] mass = fillWithRandoms(size);
         double[] finalMass = mass.clone();
-        massTest(finalMass, 15);
-        printArrays(mass, finalMass, 14);
-        //System.out.println(Arrays.toString(mass));
-        //System.out.println(Arrays.toString(finalMass));
+        replaceWithZero(finalMass, index);
+        printArrays(mass, finalMass, index);
+
+        finalMass = mass.clone();
+        index = 15;
+        replaceWithZero(finalMass, index);
+        printArrays(mass, finalMass, index);
+
+        finalMass = mass.clone();
+        index = 0;
+        replaceWithZero(finalMass, index);
+        printArrays(mass, finalMass, index);
+
+        finalMass = mass.clone();
+        index = 14;
+        replaceWithZero(finalMass, index);
+        printArrays(mass, finalMass, index);
     }
 
     public static double[] fillWithRandoms(int size) {
@@ -23,8 +38,8 @@ public class ElementsRemover {
         return array;
     }
 
-    public static double[] massTest(double[] finalMass, int index) {
-        if (checkIndex(finalMass, index) == true) {
+    public static double[] replaceWithZero(double[] finalMass, int index) {
+        if (checkIndex(finalMass, index)) {
             return finalMass;
         } else {
             for (int i = ++index; i < finalMass.length; i++) {
@@ -35,38 +50,43 @@ public class ElementsRemover {
     }
 
     public static void printArrays(double[] mass, double[] finalMass, int index) {
-        if (checkIndex(mass, index) == false) {
+        if (!checkIndex(mass, index)) {
+            System.out.println("\nИсходный масив:");
             for (int i = 0; i < 8; i++) {
-                System.out.print(mass[i] + " ");
+                System.out.printf("%.3f ", mass[i]);
             }
             System.out.println();
             for (int i = 8; i < mass.length; i++) {
-                System.out.print(mass[i] + " ");
+                System.out.printf("%.3f ", mass[i]);
             }
-            System.out.println("\n\n");
+
+            System.out.println("\n\nИзмененный массив:");
+
             for (int i = 0; i < 8; i++) {
-                System.out.print(finalMass[i] + " ");
+                System.out.printf("%.3f ", finalMass[i]);
             }
             System.out.println();
             for (int i = 8; i < mass.length; i++) {
-                System.out.print(finalMass[i] + " ");
+                System.out.printf("%.3f ", finalMass[i]);
             }
 
-            System.out.println("\n\n" + index);
-        }
-    }
-
-    public static boolean checkIndex(double[] mass, int index) {
-        boolean check = false;
-        if (index < 0 || index > mass.length - 1) {
+            System.out.println("\n\nПереданный индекс: " + index);
+            System.out.printf("Значение ячейки из переданного индекса: %.3f ", finalMass[index]);
+        } else {
             int i = 0;
             int[] indexes = new int[mass.length];
             for (double ind : mass) {
                 indexes[i] += i;
                 i++;
             }
-            //System.out.println("Индекс не входит в границы");
-            //System.out.println("Передан индекс " + index + " допустимые индексы " + Arrays.toString(indexes));
+            System.out.println("Индекс не входит в границы");
+            System.out.println("Передан индекс " + index + " допустимые индексы " + Arrays.toString(indexes) + "\n");
+        }
+    }
+
+    public static boolean checkIndex(double[] mass, int index) {
+        boolean check = false;
+        if (index < 0 || index > mass.length - 1) {
             check = true;
         }
         return check;
